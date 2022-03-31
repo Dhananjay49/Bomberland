b0fc52ae01af918762cb2aae7fc696a945e1108c
Dhananjay49 <44177299+Dhananjay49@users.noreply.github.com>
2022-03-31 22:20:29 +0530

    Add files via upload

--- cpp/agent.cpp
+++ cpp/agent.cpp
@@ -26,14 +26,23 @@ private:
   static int distances[225][3];
   static int parent[225][3];
   static bool visited[225][3];
+  static int bomb[225];
+  static int explosion[225];
+  static int parent_bomb[225];
+  static int safe_grid[15][15];
+  static int bomb_created[225];
   static std::map<std::string,int> unit_string_num;
+  static vector<int> path_bombs, kill_bombs;
 
 public:
   static int last_spiral_not_occupied(int bl);
   static bool che(int r,int c);
-  static void fill_metal_wood(const json& game_state);
+  static void fill_metal_wood_bomb(const json& game_state, int tick);
   static int dist(int bl,int bl1);
   static void update_distances_parents(int unit_id, int bl);
+  static int closest_safe_reachable_location(int unit_id,int bl);
+  static bool is_safe_unit(int unit_id,int unit_pos,int bomb_pos);
+  static void fill_safe_grid();
   static void run();
 };
 
@@ -43,13 +52,20 @@ std::string Agent::their_agent_id;
 std::vector<std::string> Agent::their_units;
 
 int Agent::tick;
+int Agent::safe_grid[15][15];
 int Agent::wood[225];
 int Agent::metal_ore[225];
 int Agent::distances[225][3];
 int Agent::unit_present[225];
 int Agent::parent[225][3];
 bool Agent::visited[225][3];
+int Agent::bomb[225];
+int Agent::explosion[225];
+int Agent::parent_bomb[225];
+int Agent::bomb_created[225];
 std::map<std::string,int> Agent::unit_string_num;
+vector<int> Agent::path_bombs;
+vector<int> Agent::kill_bombs;
 
 
 int Agent::last_spiral_not_occupied(int bl){
@@ -83,10 +99,14 @@ bool Agent::che(int r, int c){
 }
 
 
-void Agent::fill_metal_wood(const json& game_state){
+void Agent::fill_metal_wood_bomb(const json& game_state,int tick){
   for(int i=0;i<225;i++){
     metal_ore[i]=0;
     wood[i]=0;
+    bomb[i]=0;
+    explosion[i]= 0;
+    parent_bomb[i]=-1;
+    bomb_created[i]=-1;
   }
   const json& entities = game_state["entities"];
       for (const auto& entity: entities) 
@@ -98,9 +118,99 @@ void Agent::fill_metal_wood(const json& game_state){
         } else if(entity["type"] == "w"){
           int x = entity["x"], y = entity["y"];
           wood[x*15+y] = 1;
+        // }
+        } else if(entity["type"] == "b"){
+          int x = entity["x"], y = entity["y"];
+          bomb[x*15+y] = entity["blast_diameter"];
+          // parent_bomb[15*x+y] = unit_string_num.find(entity["owner_unit_id"])!=unit_string_num.end()?
+          //                               unit_string_num[entity["owner_unit_id"]]:-1;
+        } else if(entity["type"] == "x"){
+          if(entity.find("expires")!=entity.end()){
+          int x = entity["x"], y = entity["y"];
+          explosion[x*15+y] = entity["expires"];
+          explosion[x*15+y] -= tick;
+          }
         }
       }
 }
+
+void Agent::fill_safe_grid(){
+  int safe_grid1[15][15];
+  int safe_grid2[15][15];
+  for(int i=0;i<15;i++){
+    for(int j=0;j<15;j++){
+      safe_grid1[i][j] = 0;
+      safe_grid2[i][j] = 0;
+      safe_grid[i][j] = 0;
+    }
+  }
+  for(int i=0;i<225;i++){
+    if(bomb[i]){
+      int row = i/15;
+      int col = i%15;
+      safe_grid1[max(row-bomb[i],0)][col]+=1;
+      if(row+bomb[i]+1<=14){
+      safe_grid1[row+bomb[i]+1][col]+=-1;}
+      safe_grid2[row][max(col-bomb[i],0)]+=1;
+      if(col+bomb[i]+1<=14){
+      safe_grid2[row][col+bomb[i]+1]+=-1;}
+    }
+  }
+  for(int i=0;i<15;i++){
+    int curr = 0,curr1=0;
+    for(int j=0;j<15;j++){
+      curr+=safe_grid2[i][j];
+      safe_grid[i][j]+=curr;
+      curr1+=safe_grid1[j][i];
+      safe_grid[j][i]+=curr1;
+    }
+  }
+
+  for(int i=0;i<225;i++){
+    if(explosion[i]){
+      int row = i/15;
+      int col = i%15;
+      safe_grid[row][col]+=1;
+    }
+  }
+
+}
+
+bool Agent::is_safe_unit(int unit_id,int unit_pos,int bomb_pos){
+  if(bomb_pos==-1){
+    return safe_grid[unit_pos/15][unit_pos%15]==0;
+  } else {
+    int row = unit_pos/15;
+    int col = unit_pos%15;
+    int bomb_row = bomb_pos/15;
+    int bomb_col = bomb_pos%15;
+    if(row<=bomb_row+bomb[bomb_pos] && row>=bomb_row-bomb[bomb_pos] && 
+    col<=bomb_col+bomb[bomb_pos] && col>=bomb_col-bomb[bomb_pos] ){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+
+int Agent::closest_safe_reachable_location(int unit_id,int bl){
+  int mini = 1e9, bl1 = bl;
+  for(int i=0;i<15;i++){
+    for(int j=0;j<15;j++){
+      if(safe_grid[i][j]==0 && !metal_ore[15*i+j] && !wood[15*i+j] && !unit_present[15*i+j]){
+        if(mini>distances[15*i+j][unit_id] && parent[15*i+j][unit_id]!=-1){
+          mini = distances[15*i+j][unit_id];
+          bl1 = 15*i+j;
+        }
+      }
+    }
+  }
+
+  return bl1;
+}
+
+
 
 
 int Agent::dist(int bl1,int bl2){
@@ -193,7 +303,8 @@ void Agent::on_game_tick(int tick_nr, const json& game_state)
 
   srand(1234567 * (my_agent_id == "a" ? 1 : 0) + tick * 100 + 13);
 
-  fill_metal_wood(game_state);
+  fill_metal_wood_bomb(game_state,tick);
+  fill_safe_grid();
 
   for(int i=0;i<225;i++){
     unit_present[i]=0;
@@ -211,6 +322,8 @@ void Agent::on_game_tick(int tick_nr, const json& game_state)
      unit_present[(int)unit["coordinates"][0]*15+(int)unit["coordinates"][1]] = 1;
   }
 
+
+
   // send each (alive) unit a random action
   for (const auto& unit_id: my_units)
   {
@@ -221,7 +334,7 @@ void Agent::on_game_tick(int tick_nr, const json& game_state)
     update_distances_parents(unit_string_num[unit_id],15*x+y);
     int id = unit_string_num[unit_id];
 
-    int curr = last_spiral_not_occupied(15*x+y);
+    int curr = is_safe_unit(id,15*x+y,-1)?last_spiral_not_occupied(15*x+y):closest_safe_reachable_location(id,15*x+y);
     cout<<curr<<'\n';
     parent[15*x+y][id]=15*x+y;
     while(parent[curr][id]!=15*x+y && parent[curr][id]!=-1){
@@ -230,7 +343,12 @@ void Agent::on_game_tick(int tick_nr, const json& game_state)
     int row = curr/15;
     int col = curr%15;
 
-    std::string action = "right";
+    if(is_safe_unit(id,15*x+y,-1) && safe_grid[row][col]!=0){
+      row = x;
+      col = y;
+    }
+
+    std::string action = "xyz";
     if(row==x-1 && col==y){
       action = "left";
     } else if(row==x+1 && col==y){
@@ -239,6 +357,10 @@ void Agent::on_game_tick(int tick_nr, const json& game_state)
       action = "down";
     } else if(row==x && col==y+1){
       action = "up";
+    }
+
+    if(wood[15*row+col]){
+      action = "bomb";
     }
     std::cout << "action: " << unit_id << " -> " << action << std::endl;
 
